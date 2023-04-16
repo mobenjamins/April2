abfddc8a2ca587a35fee5fb41a174d238fc40a66
Benjamin Oghene <benjamin.oghene@thecozm.com>
2023-04-16 12:57:58 +0100

    added css and javacscript

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-8fm!xr&wm^jl()+$(bp9u^86j3*0*^x1ew1j&fim7p!ak_$wa_
 DEBUG = True
 
 ALLOWED_HOSTS = ['13.41.203.3', 'cozmtravel.com',
-                 'www.cozmtravel.com', '127.0.0.1']
+ 'www.cozmtravel.com', '127.0.0.1']
+
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
